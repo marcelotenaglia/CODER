519b92b679c5b1447392ee208a81f01d204e49dc
marcelotenaglia <tenaco23@gmail.com>
2024-02-01 19:15:53 -0300

    acomodo clase 3

--- clase3/clase3/src/App.jsx
+++ clase3/clase3/src/App.jsx
@@ -1,28 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Cards from './components/Cards'
-import AlexaImg from "./img/alexa.png"
-import SiriImg from "./img/siri.png"
-import CortanaImg from "./img/cortana.png"
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Cards from "./components/Cards";
+import AlexaImg from "./img/alexa.png";
+import SiriImg from "./img/siri.png";
+import CortanaImg from "./img/cortana.png";
+import "bulma/css/bulma.css";
 
 function App() {
-
-  
-  
-
   return (
-
     <div>
+      <section className="hero is-link">
+        <div className="hero-body">
+          <h1 className="title">Asistentes</h1>
+        </div>
+      </section>
 
-      <h1>Asistentes</h1>
-      <Cards title="Alexa" arroba="@ThisIsAlexa" img={AlexaImg}/>
-      <Cards title="Siri" arroba="@ImSiri" img={SiriImg}/>
-      <Cards title="Cortana" arroba="@HelloSoyCortana" img={CortanaImg}/>
+      <div className="container">
+        <section className="columns">
+          <div className="column is-4">
+            <Cards title="Alexa" arroba="@ThisIsAlexa" img={AlexaImg} />
+          </div>
 
+          <div className="column is-4">
+            <Cards title="Siri" arroba="@ImSiri" img={SiriImg} />
+          </div>
+
+          <div className="column is-4">
+            <Cards title="Cortana" arroba="@HelloSoyCortana" img={CortanaImg} />
+          </div>
+        </section>
+      </div>
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
--- clase3/clase3/src/components/Cards.jsx
+++ clase3/clase3/src/components/Cards.jsx
@@ -1,17 +1,53 @@
-function Cards ({title, arroba, img}) {
+import { useState } from "react";
 
-    
+function Cards({ title, arroba, img }) {
+  const [like, setLike] = useState(0); //like : variable de estado, setLike : setter
 
-    return (
-        
-       <div>
-        <img src={img}/>
-        <h3>{title}</h3>
-        <p>{arroba}</p>
-       </div>
-        
+  function handleClickLike() {
+    setLike(like + 1);
+  }
 
-    )
+  function handleClickDislike() {
+    setLike(like - 1);
+  }
+
+  return (
+    <div className="card ">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={img} />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media-content">
+          <h3 className="title">{title}</h3>
+          <p className="subtitle">{arroba}</p>
+
+          <div className="columns">
+            <div className="column">
+              <button
+                onClick={handleClickLike}
+                className="button is-info is fullwidth"
+              >
+                +
+              </button>
+            </div>
+            <div className="column">
+              <button
+                onClick={handleClickDislike}
+                className="button is-info is fullwidth"
+              >
+                -
+              </button>
+            </div>
+          </div>
+
+          <p> likes : {like} </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Cards
+export default Cards;
